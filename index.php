ab8792fbc8d7063c7a22b2f10f0e7a502ad8f1e6
heaveninteractivenew <info@heaven-interactive.com>
2025-02-08 12:59:10 +0200

    Update and rename index.php.txt to index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists(__DIR__.'/storage/framework/maintenance.php')) {
+    require __DIR__.'/storage/framework/maintenance.php';
 }
 
 /*
@@ -31,35 +31,33 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it into
 | the script here so that we don't have to worry about manual loading any
-| of our classes later on. It feels great to relax.
+| of our classes later on.
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
 |--------------------------------------------------------------------------
 |
-| We need to illuminate PHP development, so let us turn on the lights.
 | This bootstraps the framework and gets it ready for use, then it will
-| load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
+| load up the application so that we can run it and send the responses
+| back to the browser.
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to the
-| client's browser allowing them to enjoy the creative and wonderful
-| application we have prepared for them.
+| Once we have the application, we can handle the incoming request through
+| the kernel, send the response back to the client's browser, and terminate
+| the request.
 |
 */
 
