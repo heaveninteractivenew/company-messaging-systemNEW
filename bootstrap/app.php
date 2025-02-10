27589e12561c17d46dd26ff737aefd12967012be
heaveninteractivenew <info@heaven-interactive.com>
2025-02-10 12:36:39 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,9 +27,10 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
 );
+
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
